36158e445dac2c5f6e57467b6ad6b01074d60fbf
백순열 <nealbaek@NealMacMini.local>
2021-03-31 16:01:31 +0900

    chore: default project settings

---
+++ GitHubAPIExample/GitHubAPIExample/Application/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  GitHubAPIExample
+//
+//  Created by 백순열 on 2021/03/31.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
